6247eb4b66f0ce405cde7b02c2625ff249c8c43e
Supriya Das <bantibuto2@gmail.com>
2026-04-29 14:01:02 +0530

    first commit

---
+++ mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-blue-500">
+      <Text className="text-6xl">Zonerra</Text>
+    </View>
+  );
+}
